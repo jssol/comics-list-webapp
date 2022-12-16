b1526a1a428bc54f962a277283ef3febde4822cd
Jonathan Sivahera <jsivahera@icloud.com>
2022-12-16 23:22:41 +0200

    Configure store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import navReducer from './navigation/navigation';
 
 export default configureStore({
   reducer: {
+    navigation: navReducer,
   },
 });
