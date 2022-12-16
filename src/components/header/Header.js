7f66c291f8b6bc6e559fd727ced2951c29f815d5
Jonathan Sivahera <jsivahera@icloud.com>
2022-12-16 17:50:39 +0200

    Build header UI

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import { FaBars, FaSearch } from 'react-icons/fa';
 
-const Header = () => <header className="Header">Header Component</header>;
+const Header = () => (
+  <header className="w-full h-14 px-6 flex text-2xl justify-between items-center">
+    <FaBars />
+    <h2 className="uppercase bg-red-500 text-4xl font-bold h-full flex flex-col justify-center items-center px-[6px]">
+      Velmar
+    </h2>
+    <FaSearch />
+  </header>
+);
 
 export default Header;
