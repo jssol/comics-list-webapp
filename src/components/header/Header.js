5b932b4ab5ef80ecaffc030656d6ff671b80f7cb
Jonathan Sivahera <jsivahera@icloud.com>
2022-12-16 23:23:03 +0200

    Add mobile navigation logic to hamburger button

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import { FaBars, FaSearch } from 'react-icons/fa';
+import { useDispatch } from 'react-redux';
+import { navOpened } from '../../redux/navigation/navigation';
 
-const Header = () => (
-  <header className="w-full h-14 px-6 flex text-2xl justify-between items-center">
-    <FaBars />
-    <h2 className="uppercase bg-red-500 text-4xl font-bold h-full flex flex-col justify-center items-center px-[6px]">
-      Velmar
-    </h2>
-    <FaSearch />
-  </header>
-);
+const Header = () => {
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(navOpened());
+  };
+
+  return (
+    <header className="w-full h-14 px-6 flex text-3xl justify-between items-center">
+      <button type="button" onClick={handleClick}>
+        <FaBars />
+      </button>
+      <h2 className="uppercase tracking-tighter bg-red-500 text-4xl font-bold h-full flex flex-col justify-center items-center px-[6px]">
+        Velmar
+      </h2>
+      <button type="button">
+        <FaSearch />
+      </button>
+    </header>
+  );
+};
 
 export default Header;
