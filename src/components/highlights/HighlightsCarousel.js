c4519cf9e67cdd59e208bfa3b5df0d458b38ea59
Jonathan Sivahera <jsivahera@icloud.com>
2022-12-17 23:44:14 +0200

    Add unique components for highlights carousel

---
+++ src/components/highlights/HighlightsCarousel.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const HighlightsCarousel = () => <section className="HighlightsCarousel w-full h-1/3 z-10 aspect-auto bg-slate-400" />;
+
+export default HighlightsCarousel;
