84857fbde6dd00a55c2daf996147944fc76aa747
Jonathan Sivahera <jsivahera@icloud.com>
2022-12-17 23:44:40 +0200

    Configure basic Highlights component structure

--- src/components/highlights/Highlights.js
+++ src/components/highlights/Highlights.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import HighlightsCarousel from './HighlightsCarousel';
 
 const Highlights = () => (
-  <section className="Highlights">Highlights Component</section>
+  <section className="relative z-0 Highlights">
+    <HighlightsCarousel />
+    <section>
+      Highlights Content
+    </section>
+  </section>
 );
 
 export default Highlights;
