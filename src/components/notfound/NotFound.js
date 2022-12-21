3abc08c25f9161193ea13bb4ec006dabf7d81358
Jonathan Sivahera <jsivahera@icloud.com>
2022-12-22 00:10:44 +0200

    Make NotFound responsive

--- src/components/notfound/NotFound.js
+++ src/components/notfound/NotFound.js
@@ -25,17 +25,17 @@ const NotFound = () => {
       {path && (
         <section
           style={styles}
-          className="flex items-start justify-end py-8 px-12 w-screen h-screen overflow-hidden NotFound"
+          className="flex items-center justify-end lg:items-start lg:justify-end py-8 px-12 w-screen h-screen overflow-hidden NotFound"
         >
           <section className="flex flex-col items-end">
-            <p className="text-6xl font-extrabold">
+            <p className="text-5xl lg:text-6xl font-extrabold">
               Not found - Hydra stole our servers
             </p>
             <NavLink
               to="/"
-              className="bg-white py-3 px-6 text-gray-800 skew-x-[-45deg] font-semibold"
+              className="bg-white py-3 px-6 mt-4 mr-3 text-gray-800 skew-x-[-20deg] font-semibold"
             >
-              Go Home
+              <div className="skew-x-[20deg]">Go Home</div>
             </NavLink>
           </section>
         </section>
