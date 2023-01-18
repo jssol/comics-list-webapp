d70b8d3f3ffbf1b48ba98fc95367fafc23e411a6
Jonathan Sivahera <jsivahera@icloud.com>
2023-01-18 16:57:19 +0200

    Add mobile carousel item

---
+++ src/components/highlights/Highlight.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
+
+const Highlight = ({ event }) => (
+  <div className="w-full h-full overflow-hidden">
+    <img
+      alt={event.title}
+      src={`${event.thumbnail.path}.${event.thumbnail.extension}`}
+      className="w-full h-48 object-cover md:hidden"
+    />
+    <div className="w-full flex flex-col justify-between px-6">
+      <h2 className="mt-6">{event.title}</h2>
+      <p className="mt-4">{event.description}</p>
+      <NavLink
+        to="/event/:id"
+        className="w-32 h-12 flex ml-2 mt-4 items-center justify-center px-4 py-2 bg-red-500 uppercase skew-x-[-20deg] font-semibold"
+      >
+        <div className="skew-x-[20deg] text-center">Read now!</div>
+      </NavLink>
+    </div>
+  </div>
+);
+
+Highlight.propTypes = {
+  event: PropTypes.shape().isRequired,
+};
+
+export default Highlight;
