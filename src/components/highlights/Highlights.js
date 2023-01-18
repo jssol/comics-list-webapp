c001c6471698e02771b860899fb0648d2fd217b4
Jonathan Sivahera <jsivahera@icloud.com>
2023-01-18 14:27:27 +0200

    Add conditional rendering based on events fetch status

--- src/components/highlights/Highlights.js
+++ src/components/highlights/Highlights.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import HighlightsCarousel from './HighlightsCarousel';
+import { fetchEvents } from '../../redux/events/events';
 
-const Highlights = () => (
-  <section className="relative z-0 Highlights">
-    <HighlightsCarousel />
-    <section>
-      Highlights Content
+const Highlights = () => {
+  const dispatch = useDispatch();
+  const eventsState = useSelector((state) => state.events);
+  useEffect(() => {
+    dispatch(fetchEvents());
+  }, [dispatch]);
+
+  const { status, events, error } = eventsState;
+
+  return (
+    <section className="relative z-0 Highlights">
+      {status === 'loading' && <div>Loading...</div>}
+      {status === 'completed' && <HighlightsCarousel events={events} />}
+      {status === 'failed' && <div>{error}</div>}
     </section>
-  </section>
-);
+  );
+};
 
 export default Highlights;
