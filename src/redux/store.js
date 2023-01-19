fe1a2f5ea4a0fd36ff11de4ef6f840ed314ec4a6
Jonathan Sivahera <jsivahera@icloud.com>
2023-01-19 09:11:25 +0200

    Add search state slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import navReducer from './navigation/navigation';
 import eventsReducer from './events/events';
 import comicsReducer from './comics/comics';
+import searchReducer from './comics/search';
 
 export default configureStore({
   reducer: {
     navigation: navReducer,
     events: eventsReducer,
     comics: comicsReducer,
+    search: searchReducer,
   },
 });
