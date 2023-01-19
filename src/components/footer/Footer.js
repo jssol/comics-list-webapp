7cb9c33c87b3bc1bd7b429b32974c464cc459b96
Jonathan Sivahera <jsivahera@icloud.com>
2023-01-19 06:55:05 +0200

    Add attritubion to Marvel

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,30 +2,60 @@
 import React from 'react';
 
 const Footer = () => (
-  <footer className="footer p-10 bg-base-200 text-base-content">
+  <footer className="footer p-7 bg-base-200 text-base-content">
     <div>
       <span className="footer-title">Services</span>
-      <a href="#link" className="link link-hover">Branding</a>
-      <a href="#link" className="link link-hover">Design</a>
-      <a href="#link" className="link link-hover">Marketing</a>
-      <a href="#link" className="link link-hover">Advertisement</a>
+      <a href="#link" className="link link-hover">
+        Branding
+      </a>
+      <a href="#link" className="link link-hover">
+        Design
+      </a>
+      <a href="#link" className="link link-hover">
+        Marketing
+      </a>
+      <a href="#link" className="link link-hover">
+        Advertisement
+      </a>
     </div>
     <div>
       <span className="footer-title">Company</span>
-      <a href="#link" className="link link-hover">About us</a>
-      <a href="#link" className="link link-hover">Contact</a>
-      <a href="#link" className="link link-hover">Jobs</a>
-      <a href="#link" className="link link-hover">Press kit</a>
+      <a href="#link" className="link link-hover">
+        About us
+      </a>
+      <a href="#link" className="link link-hover">
+        Contact
+      </a>
+      <a href="#link" className="link link-hover">
+        Jobs
+      </a>
+      <a href="#link" className="link link-hover">
+        Press kit
+      </a>
     </div>
     <div>
       <span className="footer-title">Legal</span>
-      <a href="#link" className="link link-hover">Terms of use</a>
-      <a href="#link" className="link link-hover">Privacy policy</a>
-      <a href="#link" className="link link-hover">Cookie policy</a>
+      <a
+        href="https://www.marvel.com/"
+        target="_blank"
+        className="link link-hover"
+        rel="noreferrer"
+      >
+        Data provided by Marvel. © 2023 MARVEL
+      </a>
+      <a href="#link" className="link link-hover">
+        Terms of use
+      </a>
+      <a href="#link" className="link link-hover">
+        Privacy policy
+      </a>
+      <a href="#link" className="link link-hover">
+        Cookie policy
+      </a>
     </div>
     <div>
       <span className="footer-title">Newsletter</span>
-      <div className="form-control w-80">
+      <div className="form-control w-80" data-theme="light">
         <label className="label" htmlFor="name">
           <span className="label-text">Enter your email address</span>
         </label>
@@ -37,7 +67,10 @@ const Footer = () => (
             placeholder="username@site.com"
             className="input input-bordered w-full pr-16"
           />
-          <button type="submit" className="btn btn-primary absolute top-0 right-0 rounded-l-none">
+          <button
+            type="submit"
+            className="btn bg-red-500 absolute top-0 right-0 rounded-l-none"
+          >
             Subscribe
           </button>
         </div>
