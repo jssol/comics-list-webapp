de319f2596d0553e5574231ebf9388663f27baf0
Jonathan Sivahera <jsivahera@icloud.com>
2023-01-30 03:44:33 +0200

    Remove unused props

--- src/components/shared/WithObserverFetch.js
+++ src/components/shared/WithObserverFetch.js
@@ -1,44 +1,28 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Spinner from './Spinner';
 import Error from './Error';
 
-const WithObserverFetch = (WrappedComponent, fetchAction, selector, props) => {
+const WithObserverFetch = (
+  WrappedComponent,
+  fetchAction,
+  selector,
+) => {
   const dispatch = useDispatch();
   const state = useSelector((state) => state[selector]);
-  const [isIntersecting, setIsIntersecting] = useState(false);
 
   const { status, error } = state;
 
   useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          setIsIntersecting(true);
-          observer.disconnect();
-        }
-      });
-    });
-    observer.observe(document.querySelector('[data-hook="with-data"]'));
-    return () => observer.disconnect();
-  }, []);
-
-  useEffect(() => {
-    if (isIntersecting && status === 'idle') {
-      dispatch(fetchAction());
-    }
-  }, [isIntersecting, status, dispatch, fetchAction]);
+    dispatch(fetchAction());
+  }, [dispatch, fetchAction]);
 
   return (
     <>
-      {status === 'loading' && (
-      <Spinner containerStyle={{ height: '38rem' }} />
-      )}
-      <div data-hook="with-data">
-        {status === 'completed' && <WrappedComponent {...props} />}
-        {status === 'failed' && <Error error={error} />}
-      </div>
+      {status === 'loading' && <Spinner containerStyle={{ height: '38rem' }} />}
+      {status === 'completed' && <WrappedComponent key={selector} />}
+      {status === 'failed' && <Error error={error} />}
     </>
   );
 };
