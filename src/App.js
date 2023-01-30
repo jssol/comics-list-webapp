5252e0c4fc38a0285ed83249ffcd84a21861e4ea
Jonathan Sivahera <jsivahera@icloud.com>
2023-01-30 05:05:04 +0200

    Fetch User if user && token are present in local storage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Outlet, useDispatch } from 'react-router-dom';
 import Header from './components/header/Header';
 import Navigation from './components/navigation/Navigation';
 import Highlights from './components/highlights/Highlights';
 import Footer from './components/footer/Footer';
+import { fetchCurrentUser } from './redux/user/user';
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    const user = localStorage.getItem('user');
+    if (token && user) {
+      dispatch(fetchCurrentUser());
+    }
+  }, [dispatch]);
+
   return (
     <div className="flex flex-col w-screen overflow-x-hidden relative">
       <Header />
@@ -15,6 +26,6 @@ function App() {
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
