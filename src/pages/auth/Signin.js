eb0e1c5fb664b8f5191d421930f0a7b2142f4ce5
Jonathan Sivahera <jsivahera@icloud.com>
2023-01-31 04:54:53 +0200

    Connect user auth frontend to backend

--- src/pages/auth/Signin.js
+++ src/pages/auth/Signin.js
@@ -2,43 +2,44 @@ import React from 'react';
 import {
   Formik, Form, Field, ErrorMessage,
 } from 'formik';
+import { useDispatch } from 'react-redux';
+import { login } from '../../redux/user/user';
 
-const Signin = () => (
-  <Formik
-    initialValues={{ email: '', password: '' }}
-    validate={(values) => {
-      const errors = {};
-      if (!values.email) {
-        errors.email = 'Required';
-      } else if (
-        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-      ) {
-        errors.email = 'Invalid email address';
-      }
-      if (!values.password) {
-        errors.password = 'Required';
-      }
-      return errors;
-    }}
-    onSubmit={(values, { setSubmitting }) => {
-      setTimeout(() => {
-        alert(JSON.stringify(values, null, 2));
-        setSubmitting(false);
-      }, 400);
-    }}
-  >
-    {({ isSubmitting }) => (
+const Signin = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <Formik
+      initialValues={{ email: '', password: '' }}
+      validate={(values) => {
+        const errors = {};
+        if (!values.email) {
+          errors.email = 'Required';
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = 'Invalid email address';
+        }
+        if (!values.password) {
+          errors.password = 'Required';
+        }
+        return errors;
+      }}
+      onSubmit={(values) => {
+        dispatch(login(JSON.stringify(values, null, 2)));
+      }}
+    >
       <Form>
         <Field type="email" name="email" placeholder="Email" />
         <ErrorMessage name="email" component="div" />
         <Field type="password" name="password" placeholder="Password" />
         <ErrorMessage name="password" component="div" />
-        <button type="submit" disabled={isSubmitting}>
+        <button type="submit">
           Sign in
         </button>
       </Form>
-    )}
-  </Formik>
-);
+    </Formik>
+  );
+};
 
 export default Signin;
--- src/pages/auth/Signup.js
+++ src/pages/auth/Signup.js
@@ -2,43 +2,62 @@ import React from 'react';
 import {
   Formik, Form, Field, ErrorMessage,
 } from 'formik';
+import { useDispatch } from 'react-redux';
+import { signup } from '../../redux/user/user';
 
-const SignupForm = () => (
-  <Formik
-    initialValues={{ email: '', password: '' }}
-    validate={(values) => {
-      const errors = {};
-      if (!values.email) {
-        errors.email = 'Required';
-      } else if (
-        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-      ) {
-        errors.email = 'Invalid email address';
-      }
-      if (!values.password) {
-        errors.password = 'Required';
-      }
-      return errors;
-    }}
-    onSubmit={(values, { setSubmitting }) => {
-      setTimeout(() => {
-        alert(JSON.stringify(values, null, 2));
-        setSubmitting(false);
-      }, 400);
-    }}
-  >
-    {({ isSubmitting }) => (
+const SignupForm = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <Formik
+      initialValues={{ email: '', password: '' }}
+      validate={(values) => {
+        const errors = {};
+        if (!values.first_name) {
+          errors.first_name = 'Required';
+        }
+        if (!values.last_name) {
+          errors.last_name = 'Required';
+        }
+        if (!values.username) {
+          errors.username = 'Required';
+        }
+        if (!values.email) {
+          errors.email = 'Required';
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = 'Invalid email address';
+        }
+        if (!values.password) {
+          errors.password = 'Required';
+        }
+        return errors;
+      }}
+      onSubmit={(values) => {
+        dispatch(signup(JSON.stringify(values, null, 2)));
+      }}
+    >
       <Form>
+        <Field type="input" name="first_name" placeholder="Fist" />
+        <ErrorMessage name="first_name" component="div" />
+        <Field type="input" name="last_name" placeholder="Last" />
+        <ErrorMessage name="last_name" component="div" />
+        <Field type="input" name="username" placeholder="@username" />
+        <ErrorMessage name="username" component="div" />
         <Field type="email" name="email" placeholder="Email" />
         <ErrorMessage name="email" component="div" />
         <Field type="password" name="password" placeholder="Password" />
         <ErrorMessage name="password" component="div" />
-        <button type="submit" disabled={isSubmitting}>
-          Sign up
+        <button
+          type="submit"
+          className="w-32 h-12 flex ml-2 hover:shadow-xl mt-4 items-center justify-center px-4 py-2 bg-red-500 text-white uppercase skew-x-[-20deg] font-semibold"
+        >
+          <span className="block skew-x-[20deg]">Sign up</span>
         </button>
       </Form>
-    )}
-  </Formik>
-);
+    </Formik>
+  );
+};
 
 export default SignupForm;
