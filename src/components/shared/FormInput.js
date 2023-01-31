c0fd13becbb836eb14946411bd342731a3232567
Jonathan Sivahera <jsivahera@icloud.com>
2023-01-31 22:22:13 +0200

    Add hover state styles

--- src/components/shared/FormInput.js
+++ src/components/shared/FormInput.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { Field, ErrorMessage } from 'formik';
 import PropTypes from 'prop-types';
+import './FormInput.scss';
 
 const FormInput = (props) => {
   const { type, name, placeholder } = props;
   return (
-    <>
-      <Field type={type} name={name} placeholder={placeholder} />
-      <ErrorMessage name={name} component="div" />
-    </>
+    <div className="w-full h-auto flex flex-col border-b-2 transition-all last:border-none border-b-current focus-within:border-b-red-500">
+      <p className="mx-4 my-2 font-semibold">{placeholder}</p>
+      <div className="w-full h-10 flex items-center justify-center relative">
+        <Field
+          type={type}
+          className="Field bg-transparent w-full h-full relative focus:outline-none px-4 py-2"
+          name={name}
+          placeholder={placeholder}
+        />
+        <ErrorMessage
+          className="bg-transparent absolute right-4 text-red-500 top-0 flex items-center justify-center bottom-0"
+          name={name}
+          component="div"
+        />
+      </div>
+    </div>
   );
 };
 
